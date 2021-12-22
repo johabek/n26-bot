29e9e1fe2d5db4f304d29fddc2509926769bbccd
Jahongir <madrimovsh24@gmail.com>
2021-12-22 16:00:23 +0500

    salom jahongir

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const TelegramBot =require('node-telegram-bot-api')
+
+const bot = new TelegramBot(process.env.TOKEN, {polling: true})
+bot.on("polling_error" , console.log)
+
+bot.on('message' , (msg) => {
+    bot.sendMessage(msg.chat.id, '' + msg.chat.username)
+})
+
