941007deef9c6e6b7c0d770d5479d4099e8b824e
Illustration <SHAXX2068@yandex.ru>
2021-12-24 16:02:16 +0500

    botni davomi

--- index.js
+++ index.js
@@ -5,7 +5,30 @@ const TelegramBot =require('node-telegram-bot-api')
 const bot = new TelegramBot(process.env.TOKEN, {polling: true})
 bot.on("polling_error" , console.log)
 
-bot.on('message' , (msg) => {
-    bot.sendMessage(msg.chat.id, '' + msg.chat.username)
-})
+const step = {
+    START   : 0,
+    NAME    : 1,
+    CONTACT : 2,
+    GENDER  : 3,
+    ADDRESS : 4
+}
 
+
+//...............Keybords...............//
+
+const startKeyboard =  {
+    inline_keyboard: [ 
+        [{ text: "Start" , callback_data: "start" }]
+    ]
+}
+
+// ...............Logic............... //
+bot.on('message', (msg) => {
+    if (msg.text === '/start') {
+       const welcome = " \n\n Salom hurmatli foydalonuvchi!!" +
+                       " \n\n Biz hammani malumotlarini yig'amiz"+
+                       " \n\n Tugmani bosing" 
+
+        bot.sendPhoto(msg.chat.id , __dirname + "/img/salom.jpg" , {caption: welcome , reply_markup: JSON.stringify(startKeyboard) } )
+    } 
+})
